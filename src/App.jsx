c1d929893e60adc4cff5781a9effbe3133ce29a3
kaiovinic <kaiovini19@hotmail.com>
2023-07-19 00:56:44 -0300

    Criação do NavBar e Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 // Pages
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <NavBar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
