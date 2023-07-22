ab34e8b8de7e31880de1757b49f93a27a63e92e9
kaiovinic <kaiovini19@hotmail.com>
2023-07-22 00:08:02 -0300

    Criação das paginas Login e Cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Login from "./pages/login/Login";
+import Register from "./pages/registrer/Register";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
         <Footer />
